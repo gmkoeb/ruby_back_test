3574757fa642aec28efafeb5ba96a84e727bf4e2
gmkoeb <gmkoeb@gmail.com>
2023-08-24 19:44:40 -0300

    Fix controller by removing the flash function

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -2,9 +2,7 @@ class CitiesController < ApplicationController
   def index
     state = params[:state]
     name_part = params[:name_part]
-    warning = 'Selecione um Estado'
-    flash(warning) if state.nil? 
-    
+
     name_part == '' && @cities = City.where(state_id: State.where(name: state).first.id)
 
     name_part != '' && @cities = City.where(state_id: State.where(name: state).first.id).where('name LIKE ?', "#{name_part}%")
